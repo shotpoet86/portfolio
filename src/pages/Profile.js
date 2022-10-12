1182c674d64e6258d3ee01d4682cffc19bc7daff
shotpoet_win10 <curlybracemaster@gmail.com>
2022-10-11 23:00:33 -0500

    -created new List component to map through listed items

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,30 @@
 import {useState, useEffect} from 'react';
+import List from '../components/List';
 import Link from '../components/Link';
 import './Profile.css';
+
 
 function Profile({userName}) {
 	const [loading, setLoading] = useState(false);
 	const [profile, setProfile] = useState({});
+
+	const items = [
+		{
+			field: 'html_url',
+			value: <Link url={profile.html_url}
+			             title={profile.html_url}/>,
+		},
+		{
+			field: 'repos_url',
+			value: <Link url={profile.repos_url}
+			             title={profile.repos_url}/>,
+		},
+		{field: 'name', value: profile.name},
+		{field: 'company', value: profile.company},
+		{field: 'location', value: profile.location},
+		{field: 'email', value: profile.email},
+		{field: 'bio', value: profile.bio},
+	];
 
 	useEffect(() => {
 		async function fetchData() {
@@ -16,9 +36,11 @@ function Profile({userName}) {
 			}
 		}
 
+		// noinspection JSIgnoredPromiseFromCall
 		fetchData();
 	}, [userName]);
 
+	// noinspection JSUnresolvedVariable
 	return (<div className="Profile-container">
 		<h2>About me</h2>
 		{loading ? (<span>Loading...</span>) : (<div>
@@ -27,27 +49,7 @@ function Profile({userName}) {
 				src={profile.avatar_url}
 				alt={profile.name}
 			/>
-			<ul>
-				<li>
-					<span>html_url:</span>
-					<Link url={profile.html_url}
-					      title={profile.html_url}/>
-				</li>
-				<li>
-					<span>repos_url:</span>
-					<Link url={profile.repos_url}
-					      title={profile.repos_url}/>
-				</li>
-				<li><span>name: </span>
-					{profile.name}</li>
-				<li><span>company: </span>
-					{profile.company}</li>
-				<li><span>location: </span>
-					{profile.location}</li>
-				<li><span>email: </span>
-					{profile.email}</li>
-				<li><span>bio: </span> {profile.bio}</li>
-			</ul>
+			<List items={items}/>
 		</div>)}
 	</div>);
 }
